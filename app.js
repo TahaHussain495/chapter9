01667bcfdbeb73b7fd667f1f72e4bf4730876dfc
Taha Hussain <thussain170305@gmail.com>
2025-02-28 16:32:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,517 @@
+// 1.
+
+
+// let cityName = prompt("Enter your city name")
+
+// if (cityName == "Karachi")
+//     console.log("Welcome to the city of lights")
+
+// else{ console.log("invalid")}
+
+// 2.
+
+
+// let gender = prompt("Enter your gender")
+
+// if (gender == "Male")
+//     console.log("Good Morning Sir")
+
+// else {
+//     console.log("invalid")
+// }
+
+
+// if (gender == "Female")
+//     console.log("Good Morning Maam")
+
+// else {
+//     console.log("invalid")
+// }
+
+// 3.
+
+// let colour = prompt("Enter the colour");
+
+// if (colour == "Red")
+// console.log("Must Stop");
+
+// else {
+//     console.log("invalid")
+// }
+
+
+// if (colour == "Yellow")
+//     console.log("Ready to move");
+    
+//     else {
+//         console.log("invalid")
+//     }
+
+    
+// if (colour == "Green")
+//     console.log("Move now");
+    
+//     else {
+//         console.log("invalid")
+//     }
+
+
+// 4.
+
+// let fuel = +prompt("Enter the remainning fuel");
+
+// if("<= 0.25")
+//    { console.log("Please refill the fuel in your car")}
+
+// else (">=0.25")
+// {console.log("it's ok")}
+
+
+// 5.
+
+
+// Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+
+// a. var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true");
+// }
+// var a = 4;
+// if (++a === 5);
+
+// b.
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+
+
+// c. var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+
+
+// // d. 
+
+// let materialcost = 20000;
+// let laborCost = 2000;
+// let totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+
+// 7 ... 
+// Guess game:
+// Store a secret number (ranging from 1 to 10) in a variable.
+// Prompt user to guess the secret number.
+// a. If user guesses the same number, show “Bingo! Correct
+// answer”.
+// b. If the guessed number +1 is the secret number, show
+// “Close enough to the correct answer”.
+
+
+// let a = +prompt("Guess the game number bw 1 to 10")
+
+// let b = 5
+
+//     if(a === b) {
+//         alert("Bingo! Correct answer");
+        
+//     } else if (b +1 === a) {
+//         alert("Close enough to the correct answer")
+
+//     } else {
+//         alert("Failed")
+//     }
+
+
+
+// // 8..
+// Write a program to check whether the given number is
+// divisible by 3. Show the message to the user if the number
+// is divisible by 3.
+
+// let num = +prompt("Write a number that is divisble by 3")
+
+// if (num % 3 === 0) {
+//     alert("Correct!")
+// } else {
+//     alert("Failed!")
+// }
+
+
+// Write a program that checks whether the given input is an
+// even number or an odd number.
+
+// let num = +prompt("Enter even or odd number")
+
+// if( num % 2 === 0) {
+//     console.log("This is even number")
+// } else if ( num % 3 === 0) {
+//     console.log("This is odd number")
+// }
+
+
+
+// // 10 
+// Write a program that takes temperature as input and
+// shows a message based on following criteria
+// a. T > 40 then “It is too hot outside.”
+// b. T > 30 then “The Weather today is Normal.”
+// c. T > 20 then “Today’s Weather is cool.”
+// d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+
+// let temp = +prompt("Enter a temprature")
+
+
+// if (temp >= 40 ) {
+//     console.log("It is too hot outside")
+// } else if ( temp >= 30) {
+//     console.log("The Weather today is Normal");
+// } else if ( temp >= 20) {
+//     console.log("Today’s Weather is cool");
+// } else if ( temp >= 10) {
+//     console.log("OMG! Today’s weather is so Cool"); 
+// }
+
+
+
+// Write a program to create a calculator for +,-,*, / & %
+// using if statements. Take the following input:
+// a. First number
+// b. Second number
+// c. Operation (+, -, *, /, %)
+// Compute & show the calculated result to user.
+
+// let a = +prompt("Enter number")
+// let b = +prompt("Enter number");
+
+// let sum = 0
+
+// if ( sum = " + ") {
+//     console.log(a + b);
+// } else if (sum = " - ") {
+//     console.log(sum)
+// } else if ( sum = a * b) {
+//     console.log(sum)
+// } else if ( sum = a / b) {
+//     console.log(sum);
+// } else if ( sum = a % b) {
+//     console.log(sum);
+    
+// }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
